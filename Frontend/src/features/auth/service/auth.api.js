9c18d285fa6b46cfd71bad326774086472e35c7d
Vedant Domde <vedantdomde28@gmail.com>
2026-08-08 00:36:34 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/features/auth/service/auth.api.js
+++ Frontend/src/features/auth/service/auth.api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://perplexity-yvl7.onrender.com",
     withCredentials: true,
 })
 
